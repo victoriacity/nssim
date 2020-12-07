16d6500f31e259994a8b2d2fd819254b613ebcc1
DragonHuang97 <34802897+DragonHuang97@users.noreply.github.com>
2020-12-07 15:52:18 +0800

    bug fix

--- splatting.py
+++ splatting.py
@@ -52,19 +52,19 @@ def render(particle_radius: ti.f32, epsilon: ti.f32): # particle_position_field,
 
         # particle center coordinate transfer
         tmp = camera.L2W @ position(pos[i]) # particle position view space 4d homogeneous coord
-        pos_view_space[i] = ti.Vector(tmp[0]/tmp[3], tmp[1]/tmp[3], tmp[2]/tmp[3]) # divide by homogeneous coord to obtain 3d view space position (x, y, z)
-        pos_img_space[i] = camera.uncook(pos_view_space) # 2d image space position (x, y) in pixel unit
+        pos_view_space[i] = ti.Vector([tmp[0]/tmp[3], tmp[1]/tmp[3], tmp[2]/tmp[3]]) # divide by homogeneous coord to obtain 3d view space position (x, y, z)
+        pos_img_space[i] = camera.uncook(pos_view_space[i]) # 2d image space position (x, y) in pixel unit
 
         # find the projected radius in image space
-        ref_view_space = ti.Vector(pos_view_space[0] + particle_radius, pos_view_space[1], pos_view_space[2])
+        ref_view_space = ti.Vector([pos_view_space[i][0] + particle_radius, pos_view_space[i][1], pos_view_space[i][2]])
         ref_img_space = camera.uncook(ref_view_space)
-        r_projected[i] = abs(ref_img_space[0] - pos_img_space[0]) # projected radius in pixel unit
+        r_projected[i] = abs(ref_img_space[0] - pos_img_space[i][0]) # projected radius in pixel unit
 
         # process projected fragments and compute depth
         for row in range(int(pos_img_space[i][1] - r_projected[i]), int(pos_img_space[i][1] + r_projected[i])):
             for column in range(int(pos_img_space[i][0] - r_projected[i]), int(pos_img_space[i][0] + r_projected[i])):
                 # discard fragment if its distance to particle center > projected radius
-                frag_view_space = camera.cook(ti.Vector(row, column, pos_view_space[i][2])) # 3d position in view space
+                frag_view_space = camera.cook(ti.Vector([row, column, pos_view_space[i][2]])) # 3d position in view space
                 dis_projected = (frag_view_space - pos_view_space[i]).norm()
                 if (dis_projected > particle_radius): continue
                 # compute depth value for valid fragment
@@ -79,18 +79,18 @@ def render(particle_radius: ti.f32, epsilon: ti.f32): # particle_position_field,
         for row in range(int(pos_img_space[i][1] - r_projected[i]), int(pos_img_space[i][1] + r_projected[i])):
             for column in range(int(pos_img_space[i][0] - r_projected[i]), int(pos_img_space[i][0] + r_projected[i])):
 
-                frag_view_space = camera.cook(ti.Vector(row, column, pos_view_space[i][2]))  # 3d position in view space
+                frag_view_space = camera.cook(ti.Vector([row, column, pos_view_space[i][2]]))  # 3d position in view space
                 dis_projected = (frag_view_space - pos_view_space[i]).norm()
 
                 if (dis_projected > particle_radius): continue
-                w1 = 1 - dis_projected / r_projected
+                w1 = 1 - dis_projected / r_projected[i]
                 depth = (pos_view_space[i][2] - ti.sqrt(particle_radius ** 2 - dis_projected ** 2))
                 w2 = (z_buffer[row, column] - depth) / epsilon
 
                 weight = w1 * w2
                 frag_w[row, column] += weight # sum weights
                 frag_c[row, column] += weight * col[i] # blend color
-                normal = ti.normalized(ti.Vector(frag_view_space[0], frag_view_space[1], depth) - pos_view_space[i])
+                normal = ti.normalized(ti.Vector([frag_view_space[0], frag_view_space[1], depth]) - pos_view_space[i])
                 frag_n[row, column] += weight * normal # blend normal
 
     # third pass: shading
@@ -124,11 +124,12 @@ if __name__ == '__main__':
     init()
 
     # render frame
-    render(1.0, 1.0)
+    render(10.0, 1.0)
 
     # display img
     gui = ti.GUI('Camera View', camera.res[0], background_color=0x000000)
-    image = (camera.img.to_numpy() * 255).astype(int)
-    image = image[:, 0] * 65536 + image[:, 1] * 256 + image[:, 2]
-    gui.set_image(image)
-    gui.show()
+    #image = (camera.img.to_numpy() * 255).astype(int)
+    #image = image[:, 0] * 65536 + image[:, 1] * 256 + image[:, 2]
+    gui.set_image(camera.img)
+    while True:
+        gui.show()
